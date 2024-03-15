70b29579a111f86874863974b8a931e5dd722010
Nitish Kumar Singh <89700883+Nitish6204@users.noreply.github.com>
2024-03-15 17:01:36 +0000

    DataType

---
+++ 02_basics_dataType/datatype.js
@@ -0,0 +1,10 @@
+//number=>2 to power 53.
+//bigint
+//string=>""
+//boolean=>true/false.
+//null=> standalone value:  console karane par object hi milta hai .
+//undefined=>console karane par undefined hi milta hai .
+//symbol => unique.
+
+console.log(typeof undefined);//undefined
+console.log(typeof null);//object
